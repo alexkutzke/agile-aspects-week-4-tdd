8578de8a280c2549e886aa0247578288fb0c10c2
giovannybrandalise <giovannybrandalise@brq.com>
2021-08-28 10:39:59 -0300

    implementacoes de valores nao permitidos

--- roman-to-decimal.js
+++ roman-to-decimal.js
@@ -1,14 +1,52 @@
 export class RomanToDecimal {
 
-    validValues = 'IVXLDCM';
+    romanToDecimalValues = {
+        'I': {
+            value: 1,
+            repeatCountAllowed: 3
+        },
+        'V': {
+            value: 5,
+            repeatCountAllowed: 1
+        },
+        'X': {
+            value: 10,
+            repeatCountAllowed: 3
+        },
+        'L': {
+            value: 50,
+            repeatCountAllowed: 1
+        },
+        'C': {
+            value: 100,
+            repeatCountAllowed: 3
+        },
+        'D': {
+            value: 500,
+            repeatCountAllowed: 1
+        },
+        'M': {
+            value: 1000,
+            repeatCountAllowed: 3
+        }
+    };
+    allowedSubtractionValues = [
+        this.romanToDecimalValues['I'].value, 
+        this.romanToDecimalValues['X'].value, 
+        this.romanToDecimalValues['C'].value
+    ];
     charsRomanValue = [];
+    decimalValue = 0;
+    lastDecimalValue = 0;
+    currentRomanValue = null;
+    currentCountCharRoman = 1;
 
     convert(romanValue) {
         if (!romanValue) {
             return 0;
         }
         this.validateValue(romanValue);
-        return 0;
+        return this.getDecimalValue();
     }
 
     validateValue(romanValue) {
@@ -29,8 +67,47 @@ export class RomanToDecimal {
 
     validateAllChars() {
         const hasInvalidChar = this.charsRomanValue
-            .some(charValue => !this.validValues.includes(charValue));
+            .some(charValue => 
+                !Object.keys(this.romanToDecimalValues).includes(charValue)
+            );
         if (hasInvalidChar) {
+            throw new Error('Invalid Roman Value');
+        }
+    }
+
+    getDecimalValue() {
+        this.charsRomanValue.reverse().forEach(charRomanValue => {
+            this.calculateDecimalValue(charRomanValue);
+        });
+        return this.decimalValue;
+    }
+
+    calculateDecimalValue(charRomanValue) {
+        this.currentRomanValue = this.romanToDecimalValues[charRomanValue];
+        this.checkRepeatCountAllowed();
+        let decimalValueForChar = this.currentRomanValue.value;
+        if (decimalValueForChar >= this.lastDecimalValue) {
+            this.decimalValue += decimalValueForChar;
+        } else {
+            this.checkAllowedSubtraction();
+            this.decimalValue -= decimalValueForChar;
+        }
+        this.lastDecimalValue = decimalValueForChar;
+    }
+
+    checkRepeatCountAllowed() {
+        if (this.lastDecimalValue === this.currentRomanValue.value) {
+            this.currentCountCharRoman++;
+        } else {
+            this.currentCountCharRoman = 1;
+        }
+        if (this.currentCountCharRoman > this.currentRomanValue.repeatCountAllowed) {
+            throw new Error('Invalid Roman Value');
+        }
+    }
+
+    checkAllowedSubtraction() {
+        if (!this.allowedSubtractionValues.includes(this.currentRomanValue.value)) {
             throw new Error('Invalid Roman Value');
         }
     }
--- roman-to-decimal.test.js
+++ roman-to-decimal.test.js
@@ -1,6 +1,8 @@
 import { RomanToDecimal } from "./roman-to-decimal";
 
-const romanToDecimal = new RomanToDecimal();
+let romanToDecimal = null;
+
+beforeEach(() => romanToDecimal = new RomanToDecimal());
 
 describe('design RomanToDecimal class', () => {
     test('exists class', () => {
@@ -19,5 +21,45 @@ describe('convert roman to decimal', () => {
     });
     test('returns exception for invalid params', () => {
         expect(() => romanToDecimal.convert('invalid')).toThrow('Invalid Roman Value');
+    });
+    test('returns 1 for I value', () => {
+        expect(romanToDecimal.convert('I')).toBe(1);
+    });
+    test('returns 5 for V value', () => {
+        expect(romanToDecimal.convert('V')).toBe(5);
+    });
+    test('returns 10 for X value', () => {
+        expect(romanToDecimal.convert('X')).toBe(10);
+    });
+    test('returns 50 for L value', () => {
+        expect(romanToDecimal.convert('L')).toBe(50);
+    });
+    test('returns 100 for C value', () => {
+        expect(romanToDecimal.convert('C')).toBe(100);
+    });
+    test('returns 500 for D value', () => {
+        expect(romanToDecimal.convert('D')).toBe(500);
+    });
+    test('returns 1000 for M value', () => {
+        expect(romanToDecimal.convert('M')).toBe(1000);
+    });
+    test('returns exception for lower case value', () => {
+        expect(() => romanToDecimal.convert('v')).toThrow('Invalid Roman Value');
+    });
+    test('returns 2 for II value', () => {
+        expect(romanToDecimal.convert('II')).toBe(2);
+    });
+    test('returns 4 for IV value', () => {
+        expect(romanToDecimal.convert('IV')).toBe(4);
+    });
+    test("returns 6 for VI value", () => {
+        expect(romanToDecimal.convert("VI")).toBe(6);
+    });
+    test("returns exception for repeated value more than allowed", () => {
+        expect(() => romanToDecimal.convert("IIII")).toThrow("Invalid Roman Value");
+        expect(() => romanToDecimal.convert("VV")).toThrow("Invalid Roman Value");
+    });
+    test("returns exception for subtraction value not allowed", () => {
+      expect(() => romanToDecimal.convert("VL")).toThrow("Invalid Roman Value");
     });
 });
