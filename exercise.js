0f11a030139117ba41fb85a58957440cd51b9890
Vanderson Mantovani <43019713+vandermnt@users.noreply.github.com>
2021-09-18 18:11:01 -0300

    feat: add function convert roman to decimal

---
+++ exercise.js
@@ -0,0 +1,30 @@
+const valuesRoman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+export function convertRomanToDecimal(value) {
+  try {
+    validateValue(value);
+
+    let decimalNumber = 0;
+
+    for (let i = 0; i < value.length; i++) {
+      for (let obj in valuesRoman) {
+        if (obj === value[i]) {
+          decimalNumber += valuesRoman[obj];
+        }
+      }
+    }
+    return decimalNumber;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function validateValue(value) {
+  if (!value) {
+    throw new Error("Value indefined!");
+  }
+
+  return true;
+}
+
+convertRomanToDecimal("XXV");
